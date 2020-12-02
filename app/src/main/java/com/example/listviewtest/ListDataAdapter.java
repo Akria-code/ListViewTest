61e1286cc02dbbbd9971fc70c74922bce0e3aafd
wm <190204291@qq.com>
2020-12-02 17:04:49 +0800

    ListView 1.listView 2.Adapter extends BaseAdapter   重写构造方法实例化 传参 （Context,Arraylist<String> data）   2.1 返回条目行数   2.2 创建布局内容/实例化布局 3.设置适配器（封装数据）

---
+++ app/src/main/java/com/example/listviewtest/ListDataAdapter.java
@@ -0,0 +1,44 @@
+package com.example.listviewtest;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class ListDataAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<String> data;
+
+    public ListDataAdapter(Context context, ArrayList<String> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {         //返回条目，总共有多少条
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //1.每个条目的返回的内容  写一个条目布局
+
+        //2.创建布局
+        View view=View.inflate(context,R.id.item_list,null);
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.listviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    //1.变量的申明
+    private ListView mListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //2.findViewById
+        mListView=findViewById(R.id.list_view);
+
+        //2.1封装数据
+        ArrayList<String> data=new ArrayList<>();
+        for (int i=0;i<1000;i++){
+            data.add(i+" ");
+        }
+        //3.设置适配器
+        mListView.setAdapter(new ListDataAdapter(this,data));
+
+        //适配器
+    }
+}
